762dc0368d21d81ea36fd751017387f2b6327c0c
angeldiaz213 <angel.diaz@cbtis265.edu.mx>
2026-03-10 14:52:37 -0600

    fin

---
+++ Funcion.dart
@@ -0,0 +1,26 @@
+void main(List<String> args) {
+  
+suma();
+nombre("YENY");
+
+String name = ministro ();
+print("El nombre de ministro es $name");
+
+}
+
+void suma (){
+
+  int a = 5;
+  int b= 5;
+  var suma = a+b;
+  print("la suma de ambos numeros es $suma");
+}void nombre (String name){
+  
+  print("Binevenido, $name.");
+
+  
+ 
+}
+String ministro(){
+  return " DAVDSITO";
+}
